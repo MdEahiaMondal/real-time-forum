c89c4f0be7c095524ffee7fa70dae51c94c68074
Eahiya <eahiamondal@gmail.com>
2020-09-18 19:23:28 +0600

    create a new notification using database and set

---
+++ app/Notifications/NewReplyNotification.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Replay;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class NewReplyNotification extends Notification
+{
+    use Queueable;
+
+    public $reply;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @param Replay $replay
+     */
+    public function __construct(Replay  $replay)
+    {
+        $this->reply = $replay;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['database'];
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            'replyBy' => $this->reply->user->name,
+            'question' => $this->reply->question->title,
+            'path' => $this->reply->question->path
+        ];
+    }
+}
