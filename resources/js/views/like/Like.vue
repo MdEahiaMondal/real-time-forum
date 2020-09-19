4d36b3765e3791feac755a8fc1761c1fb6bbb83b
Eahiya <eahiamondal@gmail.com>
2020-09-19 08:13:25 +0600

    successfully count like by pusher in realtime

--- resources/js/views/like/Like.vue
+++ resources/js/views/like/Like.vue
@@ -1,38 +1,44 @@
 <template>
-<div>
-    <v-btn icon @click="likeReplay">
-        <v-icon :color="color">mdi-heart</v-icon> {{ count }}
-    </v-btn>
-</div>
+    <div>
+        <v-btn icon @click="likeReplay">
+            <v-icon :color="color">mdi-heart</v-icon>
+            {{ count }}
+        </v-btn>
+    </div>
 </template>
 
 <script>
 export default {
-name: "Like",
+    created() {
+        Echo.channel('likeChannel')
+            .listen('LikeEvent', (e) => {
+                if (this.reply.id === e.id){
+                    e.type === 1 ? this.count++ : this.count--
+                }
+            });
+    },
+    name: "Like",
     props: ['reply'],
-    data()
-    {
+    data() {
         return {
             count: this.reply.all_likes,
             like: this.reply.auth_liked
         }
     },
-computed: {
-    color()
-    {
-        return this.like ? 'red' : 'red lighten-2'
-    }
-},
+    computed: {
+        color() {
+            return this.like ? 'red' : 'red lighten-2'
+        }
+    },
     methods: {
-        likeReplay()
-        {
+        likeReplay() {
             this.like ? this.dec() : this.inc()
             this.like = !this.like
         },
         dec() {
             axios.delete(`http://localhost:8000/api/replies/${this.reply.id}/likes`)
                 .then(res => {
-                    this.count --
+                    this.count--
                 })
                 .catch(e => {
                     console.log(e.response.data.message)
@@ -42,7 +48,7 @@ computed: {
         inc() {
             axios.post(`http://localhost:8000/api/replies/${this.reply.id}/likes`)
                 .then(res => {
-                    this.count ++
+                    this.count++
                 })
                 .catch(e => {
                     console.log(e.response.data.message)
